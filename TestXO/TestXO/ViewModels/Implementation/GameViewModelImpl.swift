0cf78e8f9f1c3d5bd2288730a5e88e8865ebfd6e
27390 <>
2019-09-26 21:15:28 +0200

    Added ViewModel interface and added basic minimal implementation to stubbed methods For reviewer: added the minimum object definition necessary so I can start writing the business logic tests as required in TDD

---
+++ TestXO/TestXO/ViewModels/Implementation/GameViewModelImpl.swift
@@ -0,0 +1,63 @@
+//
+//  GameViewModelImpl.swift
+//  TestXO
+
+
+import Foundation
+
+class GameViewModelImpl {
+    
+    //MARK: Private variables
+    private var gameModel: GameModel
+    
+    //MARK: Type definitions
+    private enum LineType {
+        case horizontal
+        case vertical
+    }
+    private enum DiagonalType {
+        case left
+        case right
+    }
+    
+    private typealias gameStatus = (gameOver: Bool, winningPositions: [Position]?, player: Player?)
+    
+    
+    //MARK: init methods
+    init(boardSize: Int) {
+        self.gameModel = GameModel(boardRowColumnSize: boardSize,
+                                   actions: [],
+                                   currentPlayer: .player1)
+    }
+    
+    
+}
+
+//MARK: implementation GameViewModel
+extension GameViewModelImpl: GameViewModel {
+    func getNumberOfGridItems() -> Int {
+        return 0
+    }
+    
+    func getNumberOfItemsPerRow() -> Int {
+        return 0
+    }
+    
+    func endTurnWithMove(position: Position, player: Player, completion: feedbackClosure?) {
+        
+    }
+    
+    func getPlayer(order: PlayerOrder) -> Player {
+        return Player.player1
+    }
+    
+    func reset() {
+        
+    }
+    
+    static func instantiate(boardSize: Int) -> GameViewModel {
+        return GameViewModelImpl(boardSize: boardSize)
+    }
+    
+    
+}
